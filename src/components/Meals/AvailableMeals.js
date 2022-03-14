c16460e9c6bb4f14822d6b2ac2f34e974d949e5c
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-14 21:19:31 +0100

    available meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -29,16 +29,17 @@ const DUMMY_MEALS = [
     },
 ];
 
-const AvailableMeals = (props) => {
+const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meal) => (
         <MealItem
-            id={meal.id}
             key={meal.id}
+            id={meal.id}
             name={meal.name}
             description={meal.description}
             price={meal.price}
         />
     ));
+
     return (
         <section className={classes.meals}>
             <Card>
